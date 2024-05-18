bf74046b7eb3ff16d8e8e19946175f8ffbd299fb
Harshith Kumar <harshith.gannavarapu23@gmail.com>
2024-05-18 11:07:52 +0530

    pages and routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import SignOut from './pages/SignOut'
+import Shop from './pages/Shop'
+import Cart from './pages/Cart'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-5xl'>app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/sign-in' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
