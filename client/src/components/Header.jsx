e67387cdafa78d107550371e0a920a6d42e10b1c
Harshith Kumar <harshith.gannavarapu23@gmail.com>
2024-05-20 11:08:18 +0530

    update user, deleter user, sign out user functions added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,10 +3,26 @@ import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai';
 import './Header.css'
 import { useSelector } from 'react-redux';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
     const { currentUser } = useSelector(state => state.user)
+    const handleSignout = async () => {
+        try {
+            const res = await fetch('/api/user/signout', {
+                method: 'POST',
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <Navbar className='border-b-2'>
             <Link
@@ -42,7 +58,7 @@ export default function Header() {
                             <Dropdown.Item>Profile</Dropdown.Item>
                         </Link>
                         <Dropdown.Divider />
-                        <Dropdown.Item >Sign out</Dropdown.Item>
+                        <Dropdown.Item onClick={handleSignout} >Sign out</Dropdown.Item>
                     </Dropdown>
                 ) : (
                     <Link to='/sign-in'>
@@ -53,7 +69,7 @@ export default function Header() {
                 )
                 }
                 <NavbarToggle />
-            </div >
+            </div>
             <Navbar.Collapse>
                 <Navbar.Link active={path === '/'} as={'div'}>
                     <Link to='/'>
@@ -87,6 +103,6 @@ export default function Header() {
                     ) : null}
                 </Navbar.Link>
             </Navbar.Collapse>
-        </Navbar >
+        </Navbar>
     )
 }
