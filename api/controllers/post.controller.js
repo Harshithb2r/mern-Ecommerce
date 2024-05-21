f9c196727f8a577d8555ba92e760a4d5c5a2d87b
Harshith Kumar <harshith.gannavarapu23@gmail.com>
2024-05-21 09:38:59 +0530

    dash board post page completed

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,3 +8,62 @@ export const CreatePost = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
+        const filters = {
+            ...(req.query.userRef && { userRef: req.query.userRef }),
+            ...(req.query.category && { Category: req.query.category }),
+            ...(req.query.slug && { Category: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { name: { $regex: req.query.searchTerm, $options: 'i' } },
+                    { description: { $regex: req.query.searchTerm, $options: 'i' } },
+                ],
+            }),
+        };
+
+        const posts = await Post.find(filters)
+            .sort({ updatedAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const totalPosts = await Post.countDocuments(filters);
+
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deletepost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to delete this post'));
+    }
+    try {
+        await Post.findByIdAndDelete(req.params.postId);
+        res.status(200).json('The post has been deleted');
+    } catch (error) {
+        next(error);
+    }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { CreatePost } from '../controllers/post.controller.js';
+import { CreatePost, deletepost, getPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, CreatePost);
+router.get('/getposts', getPosts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 export default router;
